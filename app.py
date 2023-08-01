2de3b3429b4831633ed44997274a02c18c528740
조윤서 <124692313+chobbong@users.noreply.github.com>
2023-08-02 02:18:37 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import os
+from dotenv import load_dotenv
+from deta import Deta
+
 import calendar  # Core Python Module
 from datetime import datetime  # Core Python Module
 
@@ -5,7 +9,29 @@ import plotly.graph_objects as go  # pip install plotly
 import streamlit as st  # pip install streamlit
 from streamlit_option_menu import option_menu  # pip install streamlit-option-menu
 
-import database as db  # local import
+
+
+load_dotenv(".env")
+DETA_KEY = os.getenv("DETA_KEY") 
+
+
+deta = Deta(DETA_KEY)
+db = deta.Base("monthly_reports")
+
+def insert_period(period, incomes, expenses, comment):
+    """Returns the report on a successful creation, otherwise raises an error"""
+    return db.put({"key": period, "incomes": incomes, "expenses": expenses, "comment": comment})
+
+
+def fetch_all_periods():
+    """Returns a dict of all periods"""
+    res = db.fetch()
+    return res.items
+
+
+def get_period(period):
+    """If not found, the function will return None"""
+    return db.get(period)
 
 # -------------- SETTINGS --------------
 incomes = ["Salary", "Blog", "Other Income"]
